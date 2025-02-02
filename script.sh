67618446db409a6716f5479e1b5bc18969625756
balatuluva <bala.tulva@gmail.com>
2025-02-02 19:35:53 +0000

    ec2 with launchtemplate and nginx on amazon linux and userdata for NLB

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 #!/bin/bash
 I=1
-sgids='sg-0664fad55261dd1fa'
-for subnet in 'subnet-0d124b5eb12011584' 'subnet-0aacfa913cba54372' 'subnet-0e595f62126dd8670'
+sgids='sg-02e48f2d6a67dae12'
+for subnet in 'subnet-058b72354f7ba061d' 'subnet-01e7ac3025886d8a0'
 do
     echo "Creating EC2 Instance in $subnet ..."
-    aws ec2 run-instances --instance-type t2.nano --launch-template LaunchTemplateId=lt-0bc152f2d8ccfde3e --security-group-ids $sgids --subnet-id $subnet --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=AWSB28-Server-'${I}'}]' >> /dev/null 2>&1
+    aws ec2 run-instances --instance-type t2.nano --launch-template LaunchTemplateId=lt-0ac526ed3ad2a0625 --security-group-ids $sgids --subnet-id $subnet --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=AWSB28-Server-'${I}'}]' >> /dev/null 2>&1
     echo "Created EC2 Machine with the name Testserver-${I}"
     I=$((I+1))
 done
