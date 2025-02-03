5121c54b4727e8a5dbdfbc3a06bbb7ecd892c10f
balatuluva <bala.tulva@gmail.com>
2025-02-03 07:40:52 +0000

    ec2 with launchtemplate and nginx on amazon linux and userdata for NLB

--- userdata.sh
+++ userdata.sh
@@ -5,3 +5,17 @@ sudo systemctl restart nginx
 sudo systemctl enable nginx
 echo "<h1>$(cat /etc/hostname)</h1>" >> /var/www/html/index.nginx-debian.html
 echo "<h1>US-EAST-1A-SERVERS</h1>" >> /var/www/html/index.nginx-debian.html
+# for NLB
+---
+
+#!/bin/bash
+sudo apt update
+sudo apt install nginx -y
+sudo sed -i 's/<h1>Welcome to nginx!<\/h1>/<h1>Welcome to Homepage<\/h1>/' /var/www/html/index.nginx-debian.html
+echo '<a href="https://www.gehana26.fun/movies/">Visit for Movies</a>' | sudo tee -a /var/www/html/index.nginx-debian.html
+echo '</div>' | sudo tee -a /var/www/html/index.nginx-debian.html
+echo '<br>' | sudo tee -a /var/www/html/index.nginx-debian.html
+echo '<a href="https://www.gehana26.fun/shows/">Visit for Shows</a>' | sudo tee -a /var/www/html/index.nginx-debian.html
+sudo systemctl restart nginx
+sudo systemctl enable nginx
+# for ALB
